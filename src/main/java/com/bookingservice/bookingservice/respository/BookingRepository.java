bc5b99dc1e89d4bc77e0da134885a87c6c0ec08a
NhungNhung02 <nhunglehong06022005@gmail.com>
2026-04-23 10:56:58 +0700

    Movie_booking

---
+++ src/main/java/com/bookingservice/bookingservice/respository/BookingRepository.java
@@ -0,0 +1,13 @@
+package com.bookingservice.bookingservice.respository;
+
+import com.bookingservice.bookingservice.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Integer> {
+    List<Booking> findByUserId(int userId);
+    List<Booking> findByMovieId(int movieId);
+    List<Booking> findByStatus(Booking.BookingStatus status);
+}
